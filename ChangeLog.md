535bbc3d823bdb9830212aa2beb592fb383c906e
WillGreen98 <will.green98@hotmail.com>
2019-02-08 09:37:12 +0000

    Repo Update - Using Stack

---
+++ ChangeLog.md
@@ -0,0 +1,3 @@
+# Changelog for University-MATHFUN-Haskell
+
+## Unreleased changes
---
+++ src/Week 1/Week_1.hs
@@ -0,0 +1,30 @@
+timesTen :: Int -> Int
+timesTen x = x * 10
+
+sumThree :: Int -> Int -> Int -> Int
+sumThree x y z = x + y + z
+
+
+areaOfCircle :: Float -> Float
+areaOfCircle radius = radius * pi^2
+
+volumeOfCylinter :: Float -> Float -> Float
+volumeOfCylinter radius height = areaOfCircle radius * height
+
+distance :: Float -> Float -> Float -> Float -> Float
+distance x1 y1 x2 y2 = sqrt((y1 - y2) ^ 2 + (x1 - x2) ^ 2)
+
+threeDifferent :: Int -> Int -> Int -> Bool
+threeDifferent x y z = x /= y && x /= z
+
+divisibleBy :: Int -> Int -> Bool
+divisibleBy x y = x `rem` y == 0
+
+isEven :: Int -> Bool
+isEven x = x `mod` 2 == 0
+
+averageThree :: Int -> Int -> Int -> Float
+averageThree a b c = fromIntegral (a + b + c) / 3
+
+absolute :: Int -> Int
+absolute =
